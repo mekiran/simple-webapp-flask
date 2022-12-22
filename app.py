095ed4beef707cf9ba4cdae603ee6fe57d876c68
mekiran <114560738+mekiran@users.noreply.github.com>
2022-12-22 15:26:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def main():
 def hello():
     return 'I am good, how about you?'
 
+@app.route('')
+def hello():
+    return 'I am in Bangalore, how about you?'
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
